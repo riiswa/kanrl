4da206ac379cc8de75b46378ece4253ca5b82d48
corentinlger <corentin.lger@gmail.com>
2024-06-28 16:49:12 +0200

    Match ddqn file with Cleanrl style

--- kaqn.py
+++ kaqn.py
@@ -16,26 +16,27 @@ from tqdm import tqdm
 from utils import set_all_seeds
 from networks import initialize_network, reg
 
-def compute_loss(
-    net,
-    target,
-    data,
-    gamma=0.99,
-):
-    observations, actions, next_observations, rewards, terminations = data
 
-    with torch.no_grad():
-        next_q_values = net(next_observations)
-        next_actions = next_q_values.argmax(dim=1)
-        next_q_values_target = target(next_observations)
-        target_max = next_q_values_target[range(len(next_q_values)), next_actions]
-        td_target = rewards.flatten() + gamma * target_max * (
-            1 - terminations.flatten()
+class Agent(nn.Module):
+    def __init__(self, env, config):
+        super().__init__()
+        self.q_network = initialize_network(
+            input_size=env.observation_space.shape[0],
+            output_size=env.action_space.n,
+            **config
+        )
+        self.target_network = initialize_network(
+            input_size=env.observation_space.shape[0],
+            output_size=env.action_space.n,
+            **config
         )
 
-    old_val = net(observations).gather(1, actions).squeeze()
-    loss = nn.functional.mse_loss(td_target, old_val)
-    return loss 
+    def update_target(self):
+        self.target_network.load_state_dict(self.q_network.state_dict())
+                
+    def update_grid_from_samples(self, observations):
+         self.q_network.update_grid_from_samples(observations)
+         self.target_network.update_grid_from_samples(observations)
 
 
 @hydra.main(config_path="conf", config_name="ddqn_config", version_base=None)
@@ -43,18 +44,10 @@ def main(config: DictConfig):
     set_all_seeds(config.seed)
     env = gym.make(config.env_id)
 
-    # TODO : Might be a cleaner way to initialize the networks
-    q_network = initialize_network(
-        input_size=env.observation_space.shape[0],
-        output_size=env.action_space.n,
-        **config)
-    target_network = initialize_network(
-        input_size=env.observation_space.shape[0],
-        output_size=env.action_space.n,
-        **config)
+    # Create a ddqn agent
+    agent = Agent(env, config)
+    agent.update_target()
 
-    target_network.load_state_dict(q_network.state_dict())
-    
     run_name = f"DDQN_{config.method}_{config.env_id}_{config.seed}_{int(time.time())}"
     writer = SummaryWriter(f"runs/{run_name}")
 
@@ -62,7 +55,7 @@ def main(config: DictConfig):
     with open(f"results/{run_name}.csv", "w") as f:
         f.write("episode,length\n")
 
-    optimizer = torch.optim.Adam(q_network.parameters(), config.learning_rate)
+    optimizer = torch.optim.Adam(agent.q_network.parameters(), config.learning_rate)
     buffer = ReplayBuffer(config.replay_buffer_capacity, env.observation_space.shape[0])
 
     writer.add_text(
@@ -72,7 +65,7 @@ def main(config: DictConfig):
     )
 
     pbar_position = 0 if HydraConfig.get().mode == HydraConfig.get().mode.RUN else HydraConfig.get().job.num
-
+    
     for episode in tqdm(range(config.n_episodes), desc=f"{run_name}", position=pbar_position):
         observation, info = env.reset()
         observation = torch.from_numpy(observation)
@@ -83,14 +76,12 @@ def main(config: DictConfig):
                 action = env.action_space.sample()
             else:
                 action = (
-                    # Changed .double to .float()
-                    q_network(observation.unsqueeze(0).float())
+                    agent.q_network(observation.unsqueeze(0).float())
                     .argmax(axis=-1)
                     .squeeze()
                     .item()
                 )
             next_observation, reward, terminated, truncated, info = env.step(action)
-            # TODO : Do we wanna keep this specific condition for CartPole env ? 
             if config.env_id == "CartPole-v1":
                 reward = -1 if terminated else 0
             next_observation = torch.from_numpy(next_observation)
@@ -105,38 +96,38 @@ def main(config: DictConfig):
         with open(f"results/{run_name}.csv", "a") as f:
             f.write(f"{episode},{episode_length}\n")
 
-        # TODO : Could maybe add all the code below in a train function ? 
         if len(buffer) >= config.batch_size:
             for _ in range(config.train_steps):
-                loss = compute_loss(
-                    q_network,
-                    target_network,
-                    buffer.sample(config.batch_size),
-                    config.gamma,
-                )
-                
+                # Compute the loss
+                observations, actions, next_observations, rewards, terminations = buffer.sample(config.batch_size)
+
+                with torch.no_grad():
+                        next_q_values = agent.q_network(next_observations)
+                        next_actions = next_q_values.argmax(dim=1)
+                        next_q_values_target = agent.target_network(next_observations)
+                        target_max = next_q_values_target[range(len(next_q_values)), next_actions]
+                        td_target = rewards.flatten() + config.gamma * target_max * (1 - terminations.flatten())
+
+                old_val = agent.q_network(observations).gather(1, actions).squeeze()
+                loss = nn.functional.mse_loss(td_target, old_val)
+
+                # Add additional reg term to the loss with KANs
                 if config.method == "KAN":
-                    # TODO : should we enable specifying args for reg (lambs ...) ? Keep it like that ? Or just remove it ? 
-                    reg_ = reg(net=q_network)
+                    reg_ = reg(net=agent.q_network)
                     loss += config.lamb * reg_
+
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
             writer.add_scalar("episode_length", episode_length, episode)
             writer.add_scalar("loss", loss, episode)
-            # TODO : See how we should handle that specific update of KAN networks 
-            if (
-                episode % 25 == 0
-                and config.method == "KAN"
-                and episode < int(config.n_episodes * (1 / 2))
-            ):
-                q_network.update_grid_from_samples(buffer.observations[: len(buffer)])
-                target_network.update_grid_from_samples(buffer.observations[: len(buffer)])
+
+            if episode % 25 == 0 and config.method == "KAN" and episode < int(config.n_episodes * (1 / 2)):
+                agent.update_grid_from_samples(buffer.observations[: len(buffer)])
 
             if episode % config.target_update_freq == 0:
-                target_network.load_state_dict(q_network.state_dict())
-
+                agent.update_target()
 
 if __name__ == "__main__":
     main()
